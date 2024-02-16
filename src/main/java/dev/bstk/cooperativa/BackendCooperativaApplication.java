7c33e23ced26753f040cbda649e8a8525504e11c
bruno.luz <bruno.luz@portoseguro.com.br>
2024-02-16 20:15:30 -0300

    main - Iniciando projeto

---
+++ src/main/java/dev/bstk/cooperativa/BackendCooperativaApplication.java
@@ -0,0 +1,13 @@
+package dev.bstk.cooperativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCooperativaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCooperativaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/bstk/cooperativa/BackendCooperativaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bstk.cooperativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCooperativaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
