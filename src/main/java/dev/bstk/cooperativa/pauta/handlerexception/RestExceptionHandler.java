ac093f9be28a0dc8aa514ea91accbea2a8d35779
bruno.luz <bruno.luz@portoseguro.com.br>
2024-02-26 12:43:35 -0300

    main - Correção bug handler exception

--- src/main/java/dev/bstk/cooperativa/pauta/handlerexception/RestExceptionHandler.java
+++ src/main/java/dev/bstk/cooperativa/pauta/handlerexception/RestExceptionHandler.java
@@ -3,10 +3,13 @@ package dev.bstk.cooperativa.pauta.handlerexception;
 import dev.bstk.cooperativa.pauta.handlerexception.exception.NaoEncontradoException;
 import dev.bstk.cooperativa.pauta.handlerexception.exception.PautaInvalidaException;
 import dev.bstk.cooperativa.pauta.handlerexception.exception.VotoInvalidoException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Map;
@@ -20,11 +23,20 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({PautaInvalidaException.class, VotoInvalidoException.class})
-    public ResponseEntity<Map<Object, Object>> exceptionUrlToken(final Exception exception) {
+    public ResponseEntity<Map<Object, Object>> invalido(final Exception exception) {
         return execute(exception, HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity<Map<Object, Object>> execute(final Exception exception, final HttpStatus status) {
+        return ResponseEntity.status(status).body(Map.of("error", new ErroResponse(exception.getMessage())));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(final HttpMessageNotReadableException ex,
+                                                                  final HttpHeaders headers,
+                                                                  final HttpStatus status,
+                                                                  final WebRequest request) {
+        final var exception = (VotoInvalidoException) ex.getCause().getCause();
         return ResponseEntity.status(status).body(Map.of("error", new ErroResponse(exception.getMessage())));
     }
 }
--- start.sh
+++ start.sh
@@ -1,9 +1,20 @@
-echo '════════════════════════════════════════════'
-echo '# CONSTRUINDO JAR                          #'
-echo '════════════════════════════════════════════'
+echo '═══════════════════'
+echo '# CONSTRUINDO JAR #'
+echo '═══════════════════'
 mvn clean install -U
 
-echo ''
+echo '═══════════════════════'
+echo '# REMOVENDO CONTAINER #'
+echo '═══════════════════════'
+docker stop rabbitmq || true && docker rm rabbitmq || true
+docker stop postgresql || true && docker rm postgresql || true
+docker stop backend-cooperativa || true && docker rm backend-cooperativa || true
+
+echo '════════════════════'
+echo '# REMOVENDO VOLUMES #'
+echo '════════════════════'
+docker volume prune -f
+
 echo '═══════════════════════════════════════════════════'
 echo '# INICIANDO TODOS OS SERVIÇOS COM DOCKER-COMPOSER #'
 echo '═══════════════════════════════════════════════════'
